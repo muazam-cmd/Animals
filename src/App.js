f5787a688856547d14d06bb01ba5d9ecaba1c166
muazam-cmd <muazamshahid7@gmail.com>
2023-01-27 12:47:26 +0400

    Using useState to make chnages in state.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 function App() {
-    return < div >
-        Show Animal List!!
-    </div >
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        console.log('Button was clicked!');
+        setCount(count + 1);
+    };
+    return (
+        <div>
+            <button onClick={handleClick}>
+                Add Animal.
+        </button >
+            <div>Number of animals: {count}</div>
+        </div>
+    );
 }
 
 export default App;

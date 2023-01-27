4f5dbf795bead0e09bcc8ac8015a9f7153efa265
muazam-cmd <muazamshahid7@gmail.com>
2023-01-27 14:52:04 +0400

    using map function to display random animals and making animalShow component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { useState } from 'react';
+import AnimalShow from './Animal.Show'
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+console.log(getRandomAnimal());
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
 
     const handleClick = () => {
-        console.log('Button was clicked!');
-        setCount(count + 1);
+        setAnimals([...animals, getRandomAnimal()])
     };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    });
     return (
         <div>
-            <button onClick={handleClick}>
-                Add Animals.
-        </button >
-            <div>Number of animals: {count}</div>
+            <button onClick={handleClick}>Add Animals.</button >
+            <div>{renderedAnimals}</div>
         </div>
     );
 }
